73199af8cb054d40bc911f41f5926b5b3e3967cb
Gabriel C. Troia <catalin.troia@gmail.com>
2016-06-21 17:17:45 -0400

    Call register from fb()

--- www/FacebookPushCampaign.js
+++ www/FacebookPushCampaign.js
@@ -1,13 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.echo = function(arg0, success, error) {
-    exec(success, error, "FacebookPushCampaign", "echo", [arg0]);
-};
-
-exports.echojs = function(arg0, success, error) {
-    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
-        success(arg0);
-    } else {
-        error('Empty message!');
-    }
+exports.register = function(arg0, success, error) {
+    exec(success, error, "FacebookPushCampaign", "register", [arg0]);
 };
