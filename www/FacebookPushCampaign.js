448aa8c4dfe47463dc2b3e35887cac6e48080170
Gabriel C. Troia <catalin.troia@gmail.com>
2016-06-21 18:41:30 -0400

    Add logging to js method

--- www/FacebookPushCampaign.js
+++ www/FacebookPushCampaign.js
@@ -1,5 +1,7 @@
 var exec = require('cordova/exec');
 
 exports.register = function(arg0, success, error) {
+    console.log("FacebookPushCampaign.register()", arg0);
+
     exec(success, error, "FacebookPushCampaign", "register", [arg0]);
 };
