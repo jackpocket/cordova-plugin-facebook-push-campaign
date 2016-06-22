92bda33323a0a04e82d283f07267b075b5dee59e
Gabriel C. Troia <catalin.troia@gmail.com>
2016-06-22 14:01:11 -0400

    Able to register the token to Facebook. (via ObjC)

--- src/ios/FacebookPushCampaign.m
+++ src/ios/FacebookPushCampaign.m
@@ -4,6 +4,30 @@
 
 @implementation FacebookPushCampaign
 
+//
+// Decodes an NSString containing hex encoded bytes into an NSData object
+// src: http://stackoverflow.com/a/14248343/2093626
+//
+- (NSData *)stringToHexData: (NSString *) str
+{
+    int len = [str length] / 2;    // Target length
+    unsigned char *buf = malloc(len);
+    unsigned char *whole_byte = buf;
+    char byte_chars[3] = {'\0','\0','\0'};
+
+    int i;
+    for (i=0; i < [str length] / 2; i++) {
+        byte_chars[0] = [str characterAtIndex:i*2];
+        byte_chars[1] = [str characterAtIndex:i*2+1];
+        *whole_byte = strtol(byte_chars, NULL, 16);
+        whole_byte++;
+    }
+
+    NSData *data = [NSData dataWithBytes:buf length:len];
+    free( buf );
+    return data;
+}
+
 - (void) register:(CDVInvokedUrlCommand *)command
 {
     NSLog(@"JP Registering");
@@ -15,14 +39,15 @@
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     } else {
 
-        NSString* deviceToken = [msg dataUsingEncoding:NSUTF8StringEncoding];
+        NSData* deviceToken = [self stringToHexData:msg];
         [FBSDKAppEvents setPushNotificationsDeviceToken:deviceToken];
 
-        NSLog(@"%@", [NSString stringWithFormat:@"JP: APN Device Token=%@", deviceToken]);
+        NSLog(@"%@", [NSString stringWithFormat:@"JP: APN Device Token =%@", msg]);
+        NSLog(@"%@", [NSString stringWithFormat:@"JP: APN Device Token As Data =%@", deviceToken]);
 
         UIAlertView *toast = [
                               [UIAlertView alloc] initWithTitle:@""
-                              message:msg
+                              message:[NSString stringWithFormat:@"Device token is: %@", deviceToken]
                               delegate:nil
                               cancelButtonTitle:nil
                               otherButtonTitles:nil, nil];
