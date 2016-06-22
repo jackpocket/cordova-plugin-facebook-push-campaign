87168c5b26cdc89bf26317534142712e8c5bccd8
Gabriel C. Troia <catalin.troia@gmail.com>
2016-06-22 15:56:32 -0400

    Refactor FacebookPushCampaign.m a bit

--- src/ios/FacebookPushCampaign.m
+++ src/ios/FacebookPushCampaign.m
@@ -31,34 +31,18 @@
 
 - (void) register:(CDVInvokedUrlCommand *)command
 {
-    NSLog(@"JP Registering");
-
     CDVPluginResult* pluginResult = nil;
     NSString* msg = [command.arguments objectAtIndex:0];
 
     if (msg == nil || [msg length] == 0) {
+        NSLog(@"FacebookPushCampaign: Register Failed");
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     } else {
 
         NSData* deviceToken = [self stringToHexData:msg];
         [FBSDKAppEvents setPushNotificationsDeviceToken:deviceToken];
 
-        NSLog(@"%@", [NSString stringWithFormat:@"JP: APN Device Token =%@", msg]);
-        NSLog(@"%@", [NSString stringWithFormat:@"JP: APN Device Token As Data =%@", deviceToken]);
-
-        UIAlertView *toast = [
-                              [UIAlertView alloc] initWithTitle:@""
-                              message:[NSString stringWithFormat:@"Device token is: %@", deviceToken]
-                              delegate:nil
-                              cancelButtonTitle:nil
-                              otherButtonTitles:nil, nil];
-
-        [toast show];
-
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC),
-                       dispatch_get_main_queue(), ^{
-                           [toast dismissWithClickedButtonIndex:0 animated:YES];
-                       });
+        NSLog(@"FacebookPushCampaign: Register Succedeed. Token = %@", deviceToken);
 
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
                                          messageAsString:msg];
@@ -70,34 +54,32 @@
 
 - (void) didReceiveRemoteNotification:(CDVInvokedUrlCommand *)command
 {
-    NSLog(@"JP didReceiveRemoteNotification");
+    NSDictionary* notification = [command.arguments objectAtIndex:0];
 
-    CDVPluginResult* pluginResult = nil;
-    NSDictionary* msg = [command.arguments objectAtIndex:0];
+    if (notification == nil) {
+        NSLog(@"FacebookPushCampaign didReceiveRemoteNotification Failed");
 
-    NSLog(@"JP msg=%@", msg);
-
-    FacebookPushCampaign* __weak weakSelf = self;
-    if (msg == nil) {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-        [self.commandDelegate sendPluginResult:pluginResult
+        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR]
                                     callbackId:command.callbackId];
     } else {
+        NSLog(@"FacebookPushCampaign didReceiveRemoteNotification %@", notification);
+
+        [FBSDKAppEvents logPushNotificationOpen:notification];
+
+        // needs to clean up by calling UIBackgroundFetchResult somehow, like in their example
+
+        FacebookPushCampaign* __weak weakSelf = self;
         FBNotificationsManager *notificationsManager = [FBNotificationsManager sharedManager];
-        [notificationsManager presentPushCardForRemoteNotificationPayload:msg
+        [notificationsManager presentPushCardForRemoteNotificationPayload:notification
                                                        fromViewController:nil
                                                                completion:^(FBNCardViewController * _Nullable viewController, NSError * _Nullable error) {
                                                                    if (error) {
-                                                                       NSLog(@"JP error");
-
-                                                                       CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-                                                                       [weakSelf.commandDelegate sendPluginResult:pluginResult
+                                                                       CDVPluginResult* pluginResultERROR = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+                                                                       [weakSelf.commandDelegate sendPluginResult:pluginResultERROR
                                                                                                    callbackId:command.callbackId];
                                                                    } else {
-                                                                       NSLog(@"JP success");
-
-                                                                       CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-                                                                       [weakSelf.commandDelegate sendPluginResult:pluginResult
+                                                                       CDVPluginResult* pluginResultOK = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                                                                       [weakSelf.commandDelegate sendPluginResult:pluginResultOK
                                                                                                        callbackId:command.callbackId];
                                                                    }
                                                                }];
