82dec698ff7fe80bae009cac76c3983f319a60a1
Gabriel C. Troia <catalin.troia@gmail.com>
2016-07-01 16:04:49 -0400

    Change API: didReceivedNotification -> didReceiveNotification

--- www/FacebookPushCampaign.js
+++ www/FacebookPushCampaign.js
@@ -3,18 +3,24 @@ var exec = require('cordova/exec');
 exports.register = function (arg0, success, error) {
   console.log("FacebookPushCampaign.register()", arg0);
 
-  exec(success, error, "FacebookPushCampaign", "register", [arg0]);
+  exec(function (token) {
+    console.log("FacebookPushCampaign.register -> success", token);
+    success && success(token);
+  }, function(error) {
+    console.error("FacebookPushCampaign.register -> error", error);
+    error && error(error)
+  }, "FacebookPushCampaign", "register", [arg0]);
 };
 
-exports.didReceivedNotification = function (arg0, success, error) {
-  console.log("FacebookPushCampaign.didReceivedNotification()", arg0);
+exports.didReceiveNotification = function (arg0, success, error) {
+  console.log("FacebookPushCampaign.didReceiveNotification()", arg0);
 
   // do some validation - make sure it adheres to the format!
   exec(function (notification) {
-    console.log('FacebookPushCampaign.didReceivedNotification -> success', notification);
+    console.log('FacebookPushCampaign.didReceiveNotification -> success', notification);
     success && success(notification);
   }, function (e) {
-    console.error('FacebookPushCampaign.didReceivedNotification -> error', e);
+    console.error('FacebookPushCampaign.didReceiveNotification -> error', e);
     error && error(e);
   }, "FacebookPushCampaign", "didReceiveRemoteNotification", [arg0]);
 };
